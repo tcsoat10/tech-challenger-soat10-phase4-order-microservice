972557a59b121fc68b68f1839fa170aa3452b3a5
Carlos Roberto <carlos.rojunior@dimensa.com.br>
2025-07-21 01:46:20 -0300

    feat: auth with bearer token openapi

--- src/app.py
+++ src/app.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from config.custom_openapi import custom_openapi
 from src.adapters.driver.api.v1.middleware.api_key_middleware import ApiKeyMiddleware
 from src.adapters.driver.api.v1.middleware.identity_map_middleware import IdentityMapMiddleware
 from src.core.containers import Container
@@ -17,6 +18,8 @@ app = FastAPI(title="Tech Challenger SOAT10 - FIAP")
 # Inicializando o container de dependências
 container = Container()
 app.container = container
+
+app.openapi = lambda: custom_openapi(app)
 
 app.add_middleware(CustomErrorMiddleware)
 app.add_middleware(AuthMiddleware)
