e8a7a5cb79928b7ae6e6d513e967253091fba0a1
LucasRamos7 <86800737+LucasRamos7@users.noreply.github.com>
2025-07-26 00:18:48 -0300

    refactor: remove unused outputs

--- terraform/infra/outputs.tf
+++ terraform/infra/outputs.tf
@@ -9,11 +9,3 @@ output "eks_cluster_endpoint" {
 output "eks_cluster_ca" {
   value = aws_eks_cluster.cluster.certificate_authority[0].data
 }
-
-output "eks_node_security_group_id" {
-  value = aws_security_group.eks_sg.id
-}
-
-output "eks_vpc_id" {
-  value = data.aws_vpc.vpc.id
-}
