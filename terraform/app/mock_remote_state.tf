7be71855b649ce3c11f5e153c1592c38cb83a4d9
LucasRamos7 <86800737+LucasRamos7@users.noreply.github.com>
2025-07-26 14:34:50 -0300

    fix: adjust mock filepath

--- terraform/app/mock_remote_state.tf
+++ terraform/app/mock_remote_state.tf
@@ -5,7 +5,7 @@ locals {
 data "terraform_remote_state" "rds" {
   backend = local.use_mock ? "local" : "s3"
   config = local.use_mock ? {
-    path = "${path.module}/mock_rds_outputs.tfstate"
+    path = "${path.module}/../mock/mock_rds_outputs.tfstate"
   } : {
     bucket = "soattc-order-db"
     key    = "env:/${terraform.workspace}/order-microservice/terraform.tfstate"
@@ -16,7 +16,7 @@ data "terraform_remote_state" "rds" {
 data "terraform_remote_state" "aws" {
   backend = local.use_mock ? "local" : "s3"
   config = local.use_mock ? {
-    path = "${path.module}/mock_eks_outputs.tfstate"
+    path = "${path.module}/../mock/mock_eks_outputs.tfstate"
   } : {
     bucket = "soattc-aws-infra"
     key    = "env:/${terraform.workspace}/order-microservice/terraform.tfstate"
