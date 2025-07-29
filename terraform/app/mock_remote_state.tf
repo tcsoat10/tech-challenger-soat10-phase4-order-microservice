155054612f5738b1d705b03bbe646937e5e03aed
LucasRamos7 <86800737+LucasRamos7@users.noreply.github.com>
2025-07-29 10:29:41 -0300

    feat: get payment and stock urls from outputs

--- terraform/app/mock_remote_state.tf
+++ terraform/app/mock_remote_state.tf
@@ -19,7 +19,30 @@ data "terraform_remote_state" "aws" {
     path = "${path.module}/../mock/mock_eks_outputs.tfstate"
   } : {
     bucket = "soattc-aws-infra"
-    key    = "env:/${terraform.workspace}/order-microservice/terraform.tfstate"
+    key    = "env:/${terraform.workspace}/aws-infra/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
+
+data "terraform_remote_state" "stock" {
+  backend = local.use_mock ? "local" : "s3"
+  config = local.use_mock ? {
+    path = "${path.module}/../mock/mock_stock_outputs.tfstate"
+  } : {
+    bucket = "soattc-stock-app"
+    key    = "env:/${terraform.workspace}/stock-microservice/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
+data "terraform_remote_state" "payment" {
+  backend = local.use_mock ? "local" : "s3"
+  config = local.use_mock ? {
+    path = "${path.module}/../mock/mock_payment_outputs.tfstate"
+  } : {
+    bucket = "soattc-payment-app"
+    key    = "env:/${terraform.workspace}/payment-microservice/terraform.tfstate"
     region = "us-east-1"
   }
 }
