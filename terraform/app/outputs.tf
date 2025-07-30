6adef267696ae9db96eccc02da61cbcc0550436e
LucasRamos7 <86800737+LucasRamos7@users.noreply.github.com>
2025-07-29 22:42:27 -0300

    refactor: add suffix to endpoint

--- terraform/app/outputs.tf
+++ terraform/app/outputs.tf
@@ -1,4 +1,4 @@
 output "order_app_lb_endpoint" {
   description = "Endpoint do Load Balancer do order-app"
-  value       = kubernetes_service.order_app_lb.status[0].load_balancer[0].ingress[0].hostname
+  value       = "{kubernetes_service.order_app_lb.status[0].load_balancer[0].ingress[0].hostname}/api/v1"
 }
